e21354dcaf1972d5cfca9bd574d86b346b2f6bc5
Jason Grout <jgrout6@bloomberg.net>
2019-07-11 10:27:54 -0500

    Fix packaging

--- setup.py
+++ setup.py
@@ -102,7 +102,6 @@ setup_args = dict(
             'recommonmark',
             'sphinx_rtd_theme',
             'nbsphinx>=0.2.13,<0.4.0',
-            'jupyter_sphinx',
             'nbsphinx-link',
             'pytest_check_links',
             'pypandoc',
